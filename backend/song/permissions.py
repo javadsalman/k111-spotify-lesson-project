e56113006851f406a1695af3ea3bdb02ede779b6
cavadsalman <cavad2013@gmail.com>
2023-06-30 12:17:24 +0400

    rest form

--- backend/song/permissions.py
+++ backend/song/permissions.py
@@ -10,3 +10,11 @@ class SongPermission(permissions.BasePermission):
             return True
         else:
             return False
+        
+    def has_object_permission(self, request, view, song):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        elif song.artists.contains(request.user.artist):
+            return True
+        else:
+            False
